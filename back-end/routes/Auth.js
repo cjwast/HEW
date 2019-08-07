83b730f5d10dde774aef530c00ab8e3aa676cc1c
Carlos <cj.wast@hotmail.com>
2019-08-06 21:19:46 -0500

    jwt implemented in bckend

--- back-end/routes/Auth.js
+++ back-end/routes/Auth.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
+const jwt = require('jsonwebtoken');
 const User = require('../models/Users');
 const bcrypt = require('bcrypt');
 const bcryptSalt = 10;
 
 //URL users/
-//GET es una consulta sencilla con todos los registros
+//GET obtiene la info detallada de un usuario
 router.get('/:id', (req, res) => {
   User.findById(req.params.id)
     .then(result => {
@@ -30,25 +31,71 @@ router.post('/', (req, res) => {
   }
 
 
-  User.findOne({ email })
+  User.findOne({ 'email': email })
     .then(user => {
-      console.log(`comparacion => ${user != null}`)
-      if (user != null) {
+      console.log(`comparacion => ${(typeof user !== 'undefined')}`)
+      if (user !== null) {
         console.log(`usuario =>${user}`)
         return res.status(401).json({ message: "the user already exist" });
       }
-    });
+      else {
+        const salt = bcrypt.genSaltSync(bcryptSalt);
+        const hashPass = bcrypt.hashSync(password, salt);
+        newUser.password = hashPass;
 
-  const salt = bcrypt.genSaltSync(bcryptSalt);
-  const hashPass = bcrypt.hashSync(password, salt);
-  newUser.password = hashPass;
-
-  newUser.save()
-    .then(userResponse => {
-      res.status(200).json(userResponse);
+        newUser.save()
+          .then(userResponse => {
+            //here we shoul return the token for that user
+            //pendiente revisar la verificación de correo??? SHIT!
+            jwt.sign({ userResponse }, 'secretkey', { expiresIn: '60s' }, (err, token) => {
+              //I really don't care the err XD
+              res.status(200).json(
+                { token }
+              );
+            })
+          })
+          .catch(err => console.log(err));
+      }
     })
     .catch(err => console.log(err));
 });
 
+
+/**Metodo para hacer el login de un usuario*/
+//URL users/login
+router.post('/login', (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (email === "" || password === "") {
+    return res.status(401).json({ message: "Indicate username and password" });
+    return;
+  }
+
+  User.findOne({ 'email': email })
+    .then(user => {
+      if (user !== null) {
+        //que si existe usuario
+        //comparamos el pasword
+        if (bcrypt.compareSync(password, user.password)) {
+          //generamos el token con jwt para responder al login
+          jwt.sign({ user }, 'secretkey', { expiresIn: '60s' }, (err, token) => {
+            //I really don't care the err XD
+            res.status(200).json(
+              { token }
+            );
+          })
+        }
+        else {
+          //mensaje ambiguo
+          return res.status(401).json({ message: "please verify the information" });
+        }
+      }
+      else {
+        //mensaje ambiguo
+        return res.status(401).json({ message: "please verify the information" });
+      }
+    })
+    .catch(err => console.log(err));
+})
 
 module.exports = router;
